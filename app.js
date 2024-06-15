6b3729838703d902718bb817c2543e5f536f972e
keneth kyeroba <kenethkyeroba@gmail.com>
2024-06-15 20:50:23 +0300

    Adding Audio and video

---
+++ app.js
@@ -0,0 +1,184 @@
+//inline comment
+
+/*
+hello
+stratcom
+communications company
+
+ */
+
+console.log("this is from javascript")
+
+// //variables
+// var name1 = "ken"
+// const nationality ="Ugandan"
+// let age = 23
+
+// console.log(name1, nationality, age)
+
+// //arithmetic perators + ,- , * /, %, //
+
+// var num1, num2;
+// num1 = 12
+// num2 = 3
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+
+// // comparisons <, > <=, >=, !==, ===
+
+// console.log(num1 < num2)
+// console.log(num1 <= num2)
+// console.log(num1 > num2)
+// console.log(num1 >= num2)
+// console.log(num1 !== num2)
+// console.log(num1 === num2)
+
+// //logical operators and && , or, not
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || false)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+
+// //conditional statements
+
+// const day ="friday"
+// if(day=== "monday"){
+//     console.log("internship day")
+
+// }else if(day==="tuesday"){
+//     console.log("go for outing")
+// }else if(day==="wednesday"){
+//     console.log("end of internship")
+// }else{
+//     console.log("Weekend vibes")
+// }
+
+
+// // conditional  statements can be nested
+
+// const num = 8;
+// if(num % 2 === 0){
+//     console.log("the number is even")
+//     if(num >10){
+//         console.log("the number is also > 10")
+//     }else{
+//         console.log("the number is < 10")
+//     }
+// }else{
+//     console.log("the number is odd")
+// }
+
+// //switch statements
+
+const num2 = 30
+
+
+// switch (num2){
+//     case 10:
+//         console.log("> 10")
+//         break
+//     case 5:
+//         console.log("< 10")
+//         break
+//     case 5:
+//         console.log("is a multiple of 2")
+//         break
+//     default:
+//         console.log("odd value and less than 10")
+
+
+// }
+
+// loops in javascript
+
+// for(let i=0; i <= 10; i++){
+//     console.log(i)
+    
+// }
+
+// // using the while loop
+// let k = 0;
+// while(k <= 10){
+//     console.log(k)
+//     k += 1
+// }
+
+// // do while loop
+
+// let count2 = 0;
+// do{
+//     console.log(count2)
+//     count2 += 1
+// }while(count2<= 10)
+
+
+
+// functions
+// its a block of code which is executed when called
+
+// const greetings = () => {
+//     const message = "hello ken, Good afternoon!"
+//     return message
+// }
+// console.log(greetings())
+
+
+// const addFunc =(x,y) =>{
+//     return x + y
+// }
+// console.log(addFunc(5,5))
+// console.log(addFunc(10,60))
+
+// CLASSES-- used for creation of objects . it starts with a capital letter
+
+// class Person{
+//     constructor(name, gender, age){
+//         this.name = name
+//         this.gender = gender
+//         this.age = age
+//     }
+//     greeting(){
+//         return `Hello ${this.name} ,good afternoon`
+//     }
+// }
+// const Person1 = new Person('ken jude', 'male', 23)
+// const Person2 = new Person('bruno', 'male', 20)
+// const Person3 = new Person('solomon', 'male', 28)
+
+
+// console.log(Person1)
+// console.log(Person2)
+// console.log(Person3)
+
+// console.log(Person1.greeting())
+// console.log(Person2.greeting())
+// console.log(Person3.greeting())
+
+// DATA STRUCTURES - OBJECTS, ARRAYS AND THEIR METHODS
+//DESTRUCTURING objects and arrays
+
+let a =100
+let b = 200
+const obj = {a: 3, b: 5, c: 4}
+({a, b} = obj);
+console.log(a,b);
+//FILTERING ARRAYS
+
+
+
+
+//DOM. DOCUMENT OBJECT MODEL
+//REACT JS
+//BACK END FRAMEWORK- Django
+//integration of back and front end
+//small application development (fully operational)
+//including authentication
+
+
